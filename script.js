0258b21f5be390e1c74101468fc04743dc110a00
Rohit Kumawat <117036470+rohitk7879@users.noreply.github.com>
2023-04-22 00:30:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let btn = document.getElementsByTagName("input")[0];
+let select = document.getElementById("colorSelect");
+btn.addEventListener('click', ()=>{
+	select.remove(select.selectedIndex);
+})  
